d1724926a1711db8072f20ff401991add3a1ed1d
Cody <codyratcliffep@gmail.com>
2023-04-09 10:01:01 -0600

    Add a form to the HTML file

---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+-create input fields for the constructor arguments
+-create a button to add a book to the library
+
+*/
+
+
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
