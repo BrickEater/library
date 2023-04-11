efec7bb4319b154e27d07acd2daec0ffc6d208b3
Cody <codyratcliffep@gmail.com>
2023-04-11 09:12:45 -0600

    Add an event listener to the form

--- script.js
+++ script.js
@@ -14,6 +14,27 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function addBookToLibrary() {
-  
+function addBookToLibrary(title, author, pages, read) {
+  let newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
 }
+
+// Get a reference to the HTML form element
+const form = document.getElementById("book-form");
+
+// Add an event listener to the form submit button
+form.addEventListener("submit", function(event) {
+  // Prevent the default form submission behavior
+  event.preventDefault();
+
+  // Get the form data
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = parseInt(document.getElementById("pages").value);
+  const read = document.getElementById("read").checked;
+
+  // Call the addBookToLibrary function with the form data as arguments
+  addBookToLibrary(title, author, pages, read);
+  console.log(myLibrary);
+});
+
