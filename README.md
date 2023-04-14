de5b83f3bc474c273669069257b1a739a14d6ffc
Cody <codyratcliffep@gmail.com>
2023-04-14 09:14:43 -0600

    Correct the submit bug

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # library
 
+14/04/2023
+So, project is done. The main thing I have to take away from this is if I'm going to use ChatGPT I need to be diligent in understanding what the bot gives me. Here's the things I took away from this project:
+- I need to make variables that target elements in the DOM
+- I need to make a function that pulls that information from the variable and places it into an array
+- If the object that is being created is common I will make a constructor and refer to it in the function
+- I need a function that displays the objects in the array
+- Distinctly different to the array is a variable that needs to be made as a payload to move the most recent book.
+    - As a side note, I suppose I could target the last object in the array instead of using this payload variable. Something like array.length.-1 or however you target the last item pushed to an array.
+- Finally, and this is a big one, I need to remember that the variables in the JS file are not the same as the elements in the DOM that they are targeting. I, for whatever reason, was looking at them as the same thing as if by saying variable = element I was creating this superposition thing. Instead, I need to realize that defining a variable is just copying and pasting the current values from the element to the variable and that they are independent of each other.
+
 13/04/2023
 Added a function that displays the books added to the library. It's adding two books for each submission, though. I am assuming that it's calling the function twice, once when there is no information and a second time when there is information in the input fields. I should use this as an opportunity to practice the debugging method that is built into the browser. I need to step through the code to see when it is doing each thing.
 
--- script.js
+++ script.js
@@ -19,27 +19,11 @@ function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(newBook);
 }
 
-// Get a reference to the HTML form element
-const form = document.getElementById("book-form");
-
-// Add an event listener to the form submit button
-form.addEventListener("submit", function(event) {
-  // Prevent the default form submission behavior
-  event.preventDefault();
-
-  // Get the form data
-  const title = document.getElementById("title").value;
-  const author = document.getElementById("author").value;
-  const pages = parseInt(document.getElementById("pages").value);
-  const read = document.getElementById("read").checked;
-
-  // Call the addBookToLibrary function with the form data as arguments
-  addBookToLibrary(title, author, pages, read);
-});
-
 function displayBooks() {
   const booksContainer = document.getElementById("books");
+  console.log(booksContainer.innerHTML);
   booksContainer.innerHTML = ""; // clear the existing content
+  console.log(booksContainer.innerHTML);
 
   myLibrary.forEach(book => {
     const bookDiv = document.createElement("div");
@@ -64,10 +48,24 @@ function displayBooks() {
   });
 }
 
-// Call the displayBooks function after adding a book to the library
+// Get a reference to the HTML form element
+const form = document.getElementById("book-form");
+
+// Add an event listener to the form submit button
 form.addEventListener("submit", function(event) {
-  // ...
+  // Prevent the default form submission behavior
+  event.preventDefault();
+
+  // Get the form data
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = parseInt(document.getElementById("pages").value);
+  const read = document.getElementById("read").checked;
+
+  // Call the addBookToLibrary function with the form data as arguments
   addBookToLibrary(title, author, pages, read);
-  console.log(myLibrary);
+  // Call the displayBooks function to display the new book
   displayBooks();
 });
+
+
